564e8002cdb5bc03485520dd9b6965f1498cd9b6
Mikhail Bratukhin <bym97m@mail.ru>
2022-12-20 11:02:20 +0300

    CORS; Code formatation
    CORS headers have been added. Code formatation.

--- fivegear.php
+++ fivegear.php
@@ -12,7 +12,7 @@ class Parser
             header("Access-Control-Allow-Origin: *");
         }
         header("Access-Control-Allow-Credentials: true");
-        header("Access-Control-Max-Age: 600");    // кэш на 10 минут
+        header("Access-Control-Max-Age: 600");
         if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
             if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
                 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
